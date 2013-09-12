60a405e98d52dbc6163a848b84fdca4e2b094e4b
sarike <sarike@timefly.cn>
2013-09-12 23:26:41 +0800

    load user error; lib unignored

--- TimeFly_Flask.py
+++ TimeFly_Flask.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask.ext.login import LoginManager
+from models.user import User
+from utils.database_session import session_cm
 from views.account.view import account
 from views.todo.views import todo
 
@@ -10,6 +12,12 @@ app.register_blueprint(todo, url_prefix='')
 app.register_blueprint(account, url_prefix='/account')
 
 login_manager = LoginManager()
+
+@login_manager.user_loader
+def load_user(user_id):
+    with session_cm() as session:
+        return session.query(User).get(user_id)
+
 login_manager.init_app(app)
 
 if __name__ == '__main__':
--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ SECRET_KEY = 'time_fly key'
 
 
 DB = {
-    'host': '127.0.0.1',
+    'host': 'localhost',
     'db_name': 'time_fly',
     'user': 'root',
     'password': 'root',
