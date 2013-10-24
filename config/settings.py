22d6022ea5579bf7c9e7ffcee47d1e4bf010cbf1
sarike <sarike@timefly.cn>
2013-10-24 20:10:38 +0800

    add openshift db support

--- config/settings.py
+++ config/settings.py
@@ -1,10 +1,25 @@
+import os
+import urlparse
+
 DEBUG = True
 SECRET_KEY = 'time_fly key'
 
 
-DB = {
-    'host': 'localhost',
-    'db_name': 'time_fly',
-    'user': 'root',
-    'password': 'root',
-}
+if 'OPENSHIFT_MYSQL_DB_URL' in os.environ:
+    url = urlparse.urlparse(os.environ.get('OPENSHIFT_MYSQL_DB_URL'))
+    DB = {
+        'db_name': os.environ['OPENSHIFT_APP_NAME'],
+        'user': url.username,
+        'password': url.password,
+        'host': url.hostname,
+        'port': url.port,
+    }
+
+else:
+    DB = {
+        'host': 'localhost',
+        'db_name': 'time_fly',
+        'user': 'root',
+        'password': 'root',
+        'port': '3306'
+    }
--- utils/database_session.py
+++ utils/database_session.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm.session import sessionmaker
 from config.settings import DB
 
 db_engine = sqlalchemy.create_engine(
-    'mysql://%s:%s@%s/%s?charset=utf8' % (DB['user'], DB['password'], DB['host'], DB['db_name']),
+    'mysql://%s:%s@%s:%s/%s?charset=utf8' % (DB['user'], DB['password'], DB['host'], DB['port'], DB['db_name']),
     echo=True,
     pool_recycle=3600,
     pool_size=15
